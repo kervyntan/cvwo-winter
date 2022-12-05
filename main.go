69ace7374a0cd9851d64bf27a60b9ad62f3adbd5
iamkervyntan <kervyntan@gmail.com>
2022-12-05 16:31:47 +0800

    added client and backend files using vite and go mod init

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+	var myName string = "Test"
+	var myName2 = "Test2"
+	myName3 := "Test3"
+
+	fmt.Printf("My life is %v and my life is %v, I like %v", myName, myName2, myName3)
+}
