80b8dfaf5d5c86d214df7aa16093133fdc3e29e2
iamkervyntan <kervyntan@gmail.com>
2022-12-05 16:54:10 +0800

    Added POST request

--- server/main.go
+++ server/main.go
@@ -7,20 +7,39 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+	Body  string `json:"body"`
+}
+
 func main() {
 	fmt.Println("Hello")
-	// var myName string = "Test"
-	// var myName2 = "Test2"
-	// myName3 := "Test3"
 
-	// // Formatted String
-	// fmt.Printf("My life is %v and my life is %v, I like %v", myName, myName2, myName3)
+	todos := []Todo{}
 	// Create an instance of the app
 	app := fiber.New()
 
 	// Get request with a callback function
+	// First endpoint
 	app.Get("/healthcheck", func(c *fiber.Ctx) error {
 		return c.SendString("THIS IS A TEST")
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		// define a variable to catch the error (err)
+		// Then test the error, if it's not nil (it is an actual error), then return the error
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.JSON(todos)
 	})
 
 	// listen to port 4000 (need the :)
