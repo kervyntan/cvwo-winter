a1882420b7c7b4ea511b3ab8d70a4128035d910b
iamkervyntan <kervyntan@gmail.com>
2022-12-06 17:47:11 +0800

    added PUT request in main.go, attempt fetch in App.tsx

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 type Todo struct {
@@ -12,6 +13,7 @@ type Todo struct {
 	Title string `json:"title"`
 	Done  bool   `json:"done"`
 	Body  string `json:"body"`
+	Test  bool   `json:"test"` // default value is false
 }
 
 func main() {
@@ -21,6 +23,10 @@ func main() {
 	// Create an instance of the app
 	app := fiber.New()
 
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://127.0.0.1:5173",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 	// Get request with a callback function
 	// First endpoint
 	app.Get("/healthcheck", func(c *fiber.Ctx) error {
@@ -39,6 +45,27 @@ func main() {
 		todo.ID = len(todos) + 1
 		todos = append(todos, *todo)
 
+		return c.JSON(todos)
+	})
+
+	app.Put("/api/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(401).SendString("Invalid Id")
+		}
+
+		for i, t := range todos {
+			if t.ID == id {
+				todos[i].Done = true
+				break
+			}
+		}
+
+		return c.JSON(todos)
+	})
+
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
 		return c.JSON(todos)
 	})
 
